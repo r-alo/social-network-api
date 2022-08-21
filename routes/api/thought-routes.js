b009a296a541c9e321878430d1e6d2772259b8af
Kevin Ralo <kevinralo@gmail.com>
2022-08-20 20:55:25 -0500

    tested basic routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -2,7 +2,45 @@ const router = require('express').Router();
 const { User, Thought } = require('../../models');
 
 router.get('/', async (req, res) => {
-  res.json({});
+  const thoughts = await Thought.find();
+  res.json(thoughts);
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const thought = await Thought.findOne({_id: id});
+  res.json(thought);
+});
+
+router.post('/', async (req, res) => {
+  const createThought = await Thought.create(req.body);
+  res.json(createThought);
+});
+
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const updatedthought = await Thought.findOneAndUpdate(
+    { _id: id },
+    { $set: req.body },
+    { runValidators: true, new: true })
+  res.json(updatedthought);
+});
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  const deletedThought = await Thought.findOneAndDelete({_id: id});
+  res.json(deletedThought);
+});
+
+router.post('/:thoughtId/reactions', async (req, res) => {
+  const createThought = await Thought.create(req.body);
+  res.json(createThought);
+});
+
+router.delete('/:thoughtId/reactions', async (req, res) => {
+  const { id } = req.params;
+  const thought = await Thought.findOne({_id: id});
+  res.json(thought);
 });
 
 module.exports = router;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -2,7 +2,58 @@ const router = require('express').Router();
 const { User, Thought } = require('../../models');
 
 router.get('/', async (req, res) => {
-  res.json({});
+  const users = await User.find();
+  res.json(users);
+});
+
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const user = await User.findOne({_id: id});
+  res.json(user);
+});
+
+router.post('/', async (req, res) => {
+  const createUser = await User.create(req.body);
+  res.json(createUser);
+});
+
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const user = await User.findOneAndUpdate(
+    { _id: id },
+    { $set: req.body },
+    { runValidators: true, new: true }
+  )
+  res.json(user);
+});
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  const findUser = await User.findOne({ _id: id });
+  await User.findOneAndDelete({ _id: findUser._id });
+
+  await Thought.find({ username: findUser.username }).remove();
+  res.json(findUser);
+});
+
+router.post('/:userId/friends/:friendId', async (req, res) => {
+  const { userId, friendId } = req.params;
+  const addFriend = await User.findOneAndUpdate(
+    { _id: userId },
+    { $addToSet: { friends: friendId } },
+    { runValidators: true, new: true },
+  );
+  res.json(addFriend);
+});
+
+router.delete('/:userId/friends/:friendId', async (req, res) => {
+  const { userId, friendId } = req.params;
+  const removeFriend = await User.findOneAndUpdate(
+    { _id: userId },
+    { $pull: { friends: friendId } },
+    { runValidators: true, new: true },
+  );
+  res.json(removeFriend);
 });
 
 module.exports = router;
